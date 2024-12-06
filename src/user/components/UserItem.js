09307b24b398483a25cb79b73a6d59892680f492
Yotam Greenstein <yotamgr314@gmail.com>
2024-12-06 19:41:34 +0200

    sadadssad
    asdasd

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -3,23 +3,26 @@ import { Link } from "react-router-dom"; // NOTE: for dynamic redirection via us
 
 import "./UserItem.css";
 import Avatar from "../../shared/components/UIElements/Avatar";
+import Card from "../../shared/components/UIElements/Card";
 
 // NOTE: each user is an li
 const UserItem = (props) => {
   return (
     <li className="user-item">
-      <div className="user-item__content">
-        <Link></Link>
-        <div className="user-item__image">
-          <Avatar image={props.image} alt={props.name} />
-        </div>
-        <div className="user-item__info">
-          <h2>{props.name}</h2>
-          <h3>
-            {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}
-          </h3>
-        </div>
-      </div>
+      <Card className="user-item__content">
+        <Link to={`/${props.id}/places`}>
+          {/*NOTE the LINK react component will take the userid from the URL path and navigate to http://localhost:3000/userId/places. NOTE it assumes we will have such frontEnd route path defined in App.js */}
+          <div className="user-item__image">
+            <Avatar image={props.image} alt={props.name} />
+          </div>
+          <div className="user-item__info">
+            <h2>{props.name}</h2>
+            <h3>
+              {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}
+            </h3>
+          </div>
+        </Link>
+      </Card>
     </li>
   );
 };
